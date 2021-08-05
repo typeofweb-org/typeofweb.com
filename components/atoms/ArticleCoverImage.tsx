d13a3589c4198a481467736dde5ffa482bef8864
Michal Miszczyszyn <michal@mmiszy.pl>
2021-08-05 17:59:11 +0200

    Update cover image

--- components/atoms/ArticleCoverImage.tsx
+++ components/atoms/ArticleCoverImage.tsx
@@ -9,21 +9,34 @@ export type CoverPlaiceholder = {
   readonly preload: boolean;
 };
 
+const GOLDEN_RATIO = (1 + Math.sqrt(5)) / 2;
+
 export const ArticleCoverImage = memo<{
   readonly cover: CoverPlaiceholder;
   readonly wide: boolean;
 }>(({ cover, wide }) => {
+  const { src, width, height } = cover.img;
+  const ratio = width / height;
+  const isLayoutFill = ratio < GOLDEN_RATIO;
+  const layoutProps = isLayoutFill
+    ? ({ layout: 'fill', objectFit: 'cover', objectPosition: 'center center' } as const)
+    : ({ layout: 'responsive', width, height } as const);
+
   return (
-    <div className={`text-[length:0] overflow-hidden ${wide ? `-mx-1 lg:-mx-4 mb-8 rounded-xl` : `rounded-t-xl`}`}>
+    <div
+      className={`text-[length:0] relative ${isLayoutFill ? 'min-h-[55vw] md:min-h-[450px]' : ''} overflow-hidden ${
+        wide ? `-mx-1 lg:-mx-4 mb-8 rounded-xl` : `rounded-t-xl`
+      }`}
+    >
       <Image
-        {...cover.img}
+        src={src}
         itemProp="image"
         itemScope
         itemType="http://schema.org/ImageObject"
         className="duration-[10s] motion-safe:hover:scale-110 bg-gray-200 transition-transform ease-in hover:ease-out"
         alt=""
         sizes="(max-width: 768px) 95vw, 768px"
-        layout="responsive"
+        {...layoutProps}
         placeholder="blur"
         blurDataURL={cover.blurDataURL}
         {...(cover.preload ? { loading: 'eager', priority: true } : { loading: 'lazy', priority: false })}
