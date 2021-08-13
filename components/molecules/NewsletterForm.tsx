3da4d5f00f4c11fec097aab075d9b176c9c84b9a
Michal Miszczyszyn <michal@mmiszy.pl>
2021-08-13 11:36:29 +0200

    Mailchimp connection

--- components/molecules/NewsletterForm.tsx
+++ components/molecules/NewsletterForm.tsx
@@ -15,36 +15,55 @@ export const NewsletterForm = memo(() => {
           Podoba się? Nie przegap kolejnych artykułów!
         </SectionTitle>
       </div>
-      <div className="flex flex-col gap-4 px-7">
-        <div className="flex flex-row gap-4">
-          <Input type="text">Imię</Input>
-          <Input type="text">Nazwisko</Input>
+      {/* <!-- Begin Mailchimp Signup Form --> */}
+      <form
+        action="https://typeofweb.us16.list-manage.com/subscribe/post?u=8073e459fa97c5444592f393a&amp;id=47bdbd08e9"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        target="_blank"
+      >
+        <div className="flex flex-col gap-4 px-7">
+          <div className="flex flex-row gap-4">
+            <Input type="text" name="FNAME" id="mce-FNAME">
+              Imię
+            </Input>
+            <Input type="text" name="LNAME" id="mce-LNAME">
+              Nazwisko
+            </Input>
+          </div>
+          <div className="flex">
+            <Input type="email" required name="EMAIL" id="mce-EMAIL">
+              Email
+            </Input>
+          </div>
+          {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
+          <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+            <input type="text" name="b_8073e459fa97c5444592f393a_47bdbd08e9" tabIndex={-1} readOnly value="" />
+          </div>
+          <div className="flex">
+            <Checkbox label="tiny" required>
+              Rozumiem i akceptuję{' '}
+              <Link href="/regulamin">
+                <a target="_blank" className="text-blue-500 hover:underline font-normal">
+                  Regulamin Newslettera
+                </a>
+              </Link>{' '}
+              oraz{' '}
+              <Link href="/polityka-prywatnosci">
+                <a target="_blank" className="text-blue-500 hover:underline font-normal">
+                  Politykę Prywatności
+                </a>
+              </Link>
+              . Wyrażam zgodę na otrzymywanie na podany adres e-mail informacji handlowych w rozumieniu ustawy z dnia 18
+              lipca 2002 r. o świadczeniu usług drogą elektroniczną.
+            </Checkbox>
+          </div>
+          <input type="hidden" readOnly value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
+          <Button type="submit">Zapisz się</Button>
         </div>
-        <div className="flex">
-          <Input type="email" required>
-            Email
-          </Input>
-        </div>
-        <div className="flex">
-          <Checkbox label="tiny" required>
-            Rozumiem i akceptuję{' '}
-            <Link href="/regulamin">
-              <a target="_blank" className="text-blue-500 hover:underline font-normal">
-                Regulamin Newslettera
-              </a>
-            </Link>{' '}
-            oraz{' '}
-            <Link href="/polityka-prywatnosci">
-              <a target="_blank" className="text-blue-500 hover:underline font-normal">
-                Politykę Prywatności
-              </a>
-            </Link>
-            . Wyrażam zgodę na otrzymywanie na podany adres e-mail informacji handlowych w rozumieniu ustawy z dnia 18
-            lipca 2002 r. o świadczeniu usług drogą elektroniczną.
-          </Checkbox>
-        </div>
-        <Button type="submit">Zapisz się</Button>
-      </div>
+      </form>
+      {/* <!--End mc_embed_signup--> */}
     </Card>
   );
 });
