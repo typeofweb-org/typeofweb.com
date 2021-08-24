9c31364c6b3d5c9f0c6d70eb29c9d748121326ac
Michal Miszczyszyn <michal@mmiszy.pl>
2021-08-24 10:43:34 +0200

    Add poweredby

--- components/molecules/SearchWidget.tsx
+++ components/molecules/SearchWidget.tsx
@@ -96,7 +96,6 @@ export const SearchWidget = memo(() => {
           </kbd>
         </div>
       </button>
-      <PoweredBy />
     </div>
   );
 });
@@ -164,6 +163,9 @@ const CustomSearchBox = connectSearchBox<CustomSearchBoxProps>(
           >
             <path d="M19.71,18.29,16,14.61A9,9,0,1,0,14.61,16l3.68,3.68a1,1,0,0,0,1.42,0A1,1,0,0,0,19.71,18.29ZM2,9a7,7,0,1,1,12,4.93h0s0,0,0,0A7,7,0,0,1,2,9Z"></path>
           </svg>
+          <div className="absolute z-10 right-3 top-1/2 -translate-y-1/2 lg:right-6">
+            <PoweredBy />
+          </div>
         </Input>
       </form>
     );
