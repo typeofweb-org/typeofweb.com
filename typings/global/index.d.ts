51e1a180b4734507a69b2773b35888028f956d33
Michal Miszczyszyn <michal@mmiszy.pl>
2021-08-25 15:57:15 +0200

    getCommentsCountCache

--- typings/global/index.d.ts
+++ typings/global/index.d.ts
@@ -2,3 +2,6 @@ declare interface Window {
   // eslint-disable-next-line functional/prefer-readonly-type -- it's mutable
   dataLayer: any[];
 }
+
+// eslint-disable-next-line no-var -- needed for a global var
+declare var getCommentsCountCache: Record<string, number>;
