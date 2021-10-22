56995f1f7b18276fbc9a745f2dec4b70640cee68
Michał Miszczyszyn <michal@mmiszy.pl>
2021-10-22 15:56:59 +0200

    Point newsletter to Ghost (#183)
    * Point newsletter to getrevue
    
    * Update signup form
    
    * Update spelling

--- components/atoms/SectionTitle.tsx
+++ components/atoms/SectionTitle.tsx
@@ -3,7 +3,7 @@ import { memo } from 'react';
 import type { PropsWithChildren, HTMLAttributes } from 'react';
 
 interface SectionTitleProps {
-  readonly size?: 'small' | 'large';
+  readonly size?: 'xs' | 'small' | 'large';
   readonly level: 1 | 2 | 'none';
   readonly itemProp?: HTMLAttributes<Element>['itemProp'];
   readonly pad?: boolean;
@@ -11,7 +11,12 @@ interface SectionTitleProps {
 
 export const SectionTitle = memo<PropsWithChildren<SectionTitleProps>>(
   ({ children, itemProp, level, size = 'large', pad = true }) => {
-    const textSize = size === 'large' ? 'text-3xl lg:text-5xl' : 'text-2xl lg:text-3xl';
+    const textSize =
+      size === 'large'
+        ? 'text-3xl lg:text-5xl font-semibold'
+        : size === 'small'
+        ? 'text-2xl lg:text-3xl font-semibold'
+        : 'text-xl lg:text-2xl';
 
     const El = level === 'none' ? 'span' : (`h${level}` as const);
 
@@ -19,7 +24,7 @@ export const SectionTitle = memo<PropsWithChildren<SectionTitleProps>>(
       <El
         className={`relative z-auto block text-center text-gray-900 font-sans hover:text-gray-700 transition-colors ${textSize} ${
           pad ? 'px-1 lg:px-4' : ''
-        } font-semibold leading-tight lg:leading-tight`}
+        } leading-tight lg:leading-tight`}
         itemProp={itemProp}
       >
         {children}
--- components/molecules/NewsletterForm.tsx
+++ components/molecules/NewsletterForm.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { memo } from 'react';
+import { memo, useCallback, useState, useEffect } from 'react';
 
 import { Button } from '../atoms/Button';
 import { Card } from '../atoms/Card';
@@ -7,39 +7,84 @@ import { Checkbox } from '../atoms/Checkbox';
 import { Input } from '../atoms/Input';
 import { SectionTitle } from '../atoms/SectionTitle';
 
+type State = 'IDLE' | 'SENDING' | 'SUCCESS' | 'ERROR';
+
 export const NewsletterForm = memo(() => {
+  const [state, setState] = useState<State>('IDLE');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = useCallback<React.FormEventHandler<HTMLFormElement>>(
+    async (e) => {
+      e.preventDefault();
+      setState('SENDING');
+
+      try {
+        const payload = {
+          name,
+          email,
+          emailType: 'subscribe',
+          labels: ['typeofweb.com'],
+          requestSrc: 'typeofweb.com',
+        };
+
+        await fetch('https://news.typeofweb.com/members/api/send-magic-link/', {
+          headers: {
+            'content-type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+          method: 'POST',
+        });
+        setState('SUCCESS');
+      } catch (err) {
+        setState('ERROR');
+      }
+    },
+    [email, name],
+  );
+
+  useEffect(() => {
+    if (state === 'ERROR' || state === 'SUCCESS') {
+      const timerId = setTimeout(() => setState('IDLE'), 5000);
+      return () => clearTimeout(timerId);
+    }
+  }, [state]);
+
+  const handleNameInput = useCallback<React.FormEventHandler<HTMLInputElement>>((e) => {
+    setName(e.currentTarget.value);
+  }, []);
+  const handleEmailInput = useCallback<React.FormEventHandler<HTMLInputElement>>((e) => {
+    setEmail(e.currentTarget.value);
+  }, []);
+
   return (
     <Card as="section" roundAllCorners={true} moreSpace={true} className="newsletter-form mx-auto max-w-xl">
       <div className="pb-4 px-5">
         <SectionTitle level="none" size="small">
-          Podoba się? Nie przegap kolejnych artykułów!
+          Zapisz się na newsletter <br />
+          „Polski frontend i backend”
+        </SectionTitle>
+        <SectionTitle level="none" size="xs">
+          aby otrzymywać najciekawsze linki do materiałów i&nbsp;wydarzeń wybrane przeze mnie!
         </SectionTitle>
       </div>
-      {/* <!-- Begin Mailchimp Signup Form --> */}
+
       <form
-        action="https://typeofweb.us16.list-manage.com/subscribe/post?u=8073e459fa97c5444592f393a&amp;id=47bdbd08e9"
+        onSubmit={handleSubmit}
+        action="https://news.typeofweb.com/members/api/send-magic-link/"
         method="post"
-        id="mc-embedded-subscribe-form"
-        name="mc-embedded-subscribe-form"
         target="_blank"
       >
         <div className="flex flex-col gap-4 px-7">
           <div className="flex flex-row gap-4">
-            <Input type="text" name="FNAME" id="mce-FNAME">
-              Imię
-            </Input>
-            <Input type="text" name="LNAME" id="mce-LNAME">
-              Nazwisko
+            <Input type="text" name="name" value={name} onInput={handleNameInput}>
+              Imię i nazwisko
             </Input>
           </div>
           <div className="flex">
-            <Input type="email" required name="EMAIL" id="mce-EMAIL">
+            <Input type="email" required name="email" value={email} onInput={handleEmailInput}>
               Email
             </Input>
-          </div>
-          {/* <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups--> */}
-          <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
-            <input type="text" name="b_8073e459fa97c5444592f393a_47bdbd08e9" tabIndex={-1} readOnly value="" />
           </div>
           <div className="flex">
             <Checkbox label="tiny" required>
@@ -55,15 +100,64 @@ export const NewsletterForm = memo(() => {
                   Politykę Prywatności
                 </a>
               </Link>
-              . Wyrażam zgodę na otrzymywanie na podany adres e-mail informacji handlowych w rozumieniu ustawy z dnia 18
+              , a także{' '}
+              <a
+                target="_blank"
+                href="https://www.getrevue.co/terms"
+                rel="noreferrer"
+                className="text-blue-500 hover:underline font-normal"
+              >
+                Regulamin
+              </a>{' '}
+              i{' '}
+              <a
+                target="_blank"
+                href="https://www.getrevue.co/privacy"
+                rel="noreferrer"
+                className="text-blue-500 hover:underline font-normal"
+              >
+                Politykę Prywatności Revue
+              </a>{' '}
+              Wyrażam zgodę na otrzymywanie na podany adres e-mail informacji handlowych w rozumieniu ustawy z dnia 18
               lipca 2002 r. o świadczeniu usług drogą elektroniczną.
             </Checkbox>
           </div>
-          <input type="hidden" readOnly value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
-          <Button type="submit">Zapisz się</Button>
+          <Button type="submit" disabled={state !== 'IDLE'}>
+            <div className="h-8">
+              {(state === 'SENDING' || state === 'SUCCESS' || state === 'ERROR') && (
+                <svg
+                  className={`w-5 h-5 text-white transition-all inline-block ${
+                    state === 'SENDING' ? 'animate-spin mx-auto' : 'mr-2 -ml-1'
+                  }`}
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  {state === 'SENDING' && (
+                    <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" />
+                  )}
+                  <path
+                    className={state === 'SENDING' ? `opacity-75` : `opacity-100`}
+                    fill="currentColor"
+                    {...(state === 'SENDING' && {
+                      d: 'M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z',
+                    })}
+                    {...(state === 'SUCCESS' && {
+                      d: 'M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z',
+                    })}
+                    {...(state === 'ERROR' && {
+                      d: 'M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z',
+                    })}
+                  />
+                </svg>
+              )}
+              {state === 'IDLE' && 'Zapisz się'}
+              {state === 'SUCCESS' && 'Udało się! Sprawdź maila :)'}
+              {state === 'ERROR' && 'Coś poszło nie tak :('}
+            </div>
+          </Button>
         </div>
       </form>
-      {/* <!--End mc_embed_signup--> */}
     </Card>
   );
 });
