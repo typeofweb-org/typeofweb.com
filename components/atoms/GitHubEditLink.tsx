b70a13926474ae6ac2ff80b3468b329bd2c64824
Michal Miszczyszyn <michal@mmiszy.pl>
2021-12-02 22:34:07 +0100

    article: Pierwsze starcia z IoT i smart home: zigbee, z-wave czy wifi?

--- components/atoms/GitHubEditLink.tsx
+++ components/atoms/GitHubEditLink.tsx
@@ -8,18 +8,18 @@ export const GitHubEditLink = ({ filePath }: { readonly filePath: string }) => {
   const url = `${repo}${filePath}`;
   return (
     <p className="mt-5 text-center text-gray-900 whitespace-nowrap text-xl font-bold">
-      <div>
+      <span className="block">
         👉{NBSP}
         {NBSP}Znalazłeś/aś błąd?{NBSP}
         {NBSP}👈
-      </div>
-      <div>
+      </span>
+      <span className="block">
         <LinkUnderlineEffect>
           <a href={url} className="text-blue-500">
             Edytuj ten wpis na GitHubie!
           </a>
         </LinkUnderlineEffect>
-      </div>
+      </span>
     </p>
   );
 };
