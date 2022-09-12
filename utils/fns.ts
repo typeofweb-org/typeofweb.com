7e9d4c90596dd7f7913091f7bd9aa2d5b0bc6292
Michal Miszczyszyn <michal@mmiszy.pl>
2022-09-12 12:56:58 +0200

    Fix build problems

--- utils/fns.ts
+++ utils/fns.ts
@@ -3,7 +3,7 @@ export function tryCatch<T>(fn: () => Promise<T>): Promise<T | Error>;
 export function tryCatch<T>(fn: () => T | Promise<T>): T | Error | Promise<T | Error> {
   try {
     const result = fn();
-    if (typeof result === 'object' && 'then' in result) {
+    if (typeof result === 'object' && result && 'then' in result) {
       return result.catch((err) => (err instanceof Error ? err : new Error(String(err))));
     } else {
       return result;
